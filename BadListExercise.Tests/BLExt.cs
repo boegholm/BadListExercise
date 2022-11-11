b5964e29fe0c0ea79d0d408ebd9fe9a77e9e67cd
boegholm <github.com@glirc.dk>
2022-11-11 12:07:53 +0100

    InsertAtTestRevealedError

---
+++ BadListExercise.Tests/BLExt.cs
@@ -0,0 +1,14 @@
+﻿namespace BadListExercise.Tests
+{
+    static class BLExt
+    {
+        public static BadList ToBadList(this int[] ia)
+        {
+            var bl = new BadList();
+            foreach (var v in ia)
+                bl.Add(v);
+            return bl;
+        }
+        public static List<int> ToList(this BadList bl) => Enumerable.Range(0, bl.Length).Select(bl.Get).ToList();
+    }
+}
--- BadListExercise.Tests/BadListTests.cs
+++ BadListExercise.Tests/BadListTests.cs
@@ -3,6 +3,7 @@ using BadListExercise;
 
 namespace BadListExercise.Tests
 {
+
     public class BadListTests
     {
         [Fact()]
@@ -12,40 +13,87 @@ namespace BadListExercise.Tests
             Assert.Equal(0, bl.Length);
         }
 
-
         [Theory]
         [InlineData(3, 3, 1, 2,3,4,3,3)]
         [InlineData(0, 25, 1, 2,3,4,3,3)]
-        public void CountNumbersTest(int expectedcount, int noi, params int[] ints)
+        public void CountNumbers_InsertRandomNumbersPlusNNoi_FindNoiExpectedCountTimes(int expectedcount, int noi, params int[] ints)
         {
-            var bl = new BadList();
-            foreach (var v in ints)
-                bl.Add(v);
+            var bl = ints.ToBadList();
             Assert.Equal(expectedcount, bl.CountNumbers(noi));
         }
 
-        [Fact()]
-        public void AddTest()
+        [Theory]
+        [InlineData(1,9,0)]
+        [InlineData(0,0,0,0)]
+        [InlineData(10,10,10,10)]
+        [InlineData(1,0,0)]
+        [InlineData(0, 0,1)]
+        public void Add_InsertRandomNumbers_ExpectInsertedInOrder(params int[] ints )
         {
-            Assert.True(false, "This test needs an implementation");
+            var bl = ints.ToBadList();
+
+            var bl_ascollection = Enumerable.Range(0,bl.Length).Select(bl.Get);
+
+            Assert.Equal(ints, bl_ascollection);            
         }
 
-        [Fact()]
-        public void GetTest()
+        [Theory]
+        [InlineData(1, 9, 0)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(10, 10, 10, 10)]
+        [InlineData(1, 0, 0)]
+        [InlineData(0, 0, 1)]
+        public void Get_InsertRandomNumbersGetAllMultipleTimes_ExpectGetIdempotent(params int[] ints)
         {
-            Assert.True(false, "This test needs an implementation");
+            var bl = ints.ToBadList();
+            List<int> bl_ascollection;
+            bl_ascollection = Enumerable.Range(0, bl.Length).Select(bl.Get).ToList();
+            bl_ascollection = Enumerable.Range(0, bl.Length).Select(bl.Get).ToList();
+            bl_ascollection = Enumerable.Range(0, bl.Length).Select(bl.Get).ToList();
+
+            Assert.Equal(ints, bl_ascollection);
         }
 
-        [Fact()]
-        public void RemoveTest()
+        [Theory]
+        [InlineData(3, 3, 1, 2, 3, 4, 3, 3)]
+        [InlineData(0, 25, 1, 2, 3, 4, 3, 3)]
+        public void Remove_RemoveRandomElement_DecreasesListSize(int randomIndex, params int[] ints)
         {
-            Assert.True(false, "This test needs an implementation");
+            var bl = ints.ToBadList();
+            var expectedLength = bl.Length - 1;
+
+            bl.Remove(randomIndex);
+
+            Assert.Equal(expectedLength, bl.Length);
         }
 
-        [Fact()]
-        public void InsertAtTest()
+        [Theory]
+        [InlineData(3, 3, 1, 2, 3, 4, 3, 3)]
+        [InlineData(0, 25, 1, 2, 3, 4, 3, 3)]
+        public void Remove_RemoveRandomElement_ContentExpectedBySTDLIBList(int randomIndex, params int[] ints)
         {
-            Assert.True(false, "This test needs an implementation");
+            var bl = ints.ToBadList();
+            var expectedResult = ints.ToList();
+            expectedResult.RemoveAt(randomIndex);
+
+            bl.Remove(randomIndex);
+
+            Assert.Equal(expectedResult , bl.ToList());
+        }
+        [Theory]
+        [InlineData(1, 0, 9, 0)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(10, 10, 10, 10,10,10,10,10,10,10,10,10,10)]
+        [InlineData(1, 0, 0)]
+        [InlineData(0, 0, 1)]
+        [InlineData(0,1)]
+        public void InsertAtTest(int n, int index, params int[] ints)
+        {
+            int expectedLength=ints.Length+1;
+            var bl = ints.ToBadList();
+            bl.InsertAt(index, n);
+
+            Assert.Multiple(()=>Assert.Equal(bl.Length))
         }
     }
 }
